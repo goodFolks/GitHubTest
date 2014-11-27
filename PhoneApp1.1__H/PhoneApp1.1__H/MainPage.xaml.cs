4306bf7413de8f300f85b7143e08fca297941339
Mohammed Omar <dante_demon89@live.com>
2014-11-27 23:12:18 +0200

    j

--- PhoneApp1.1__H/PhoneApp1.1__H/MainPage.xaml.cs
+++ PhoneApp1.1__H/PhoneApp1.1__H/MainPage.xaml.cs
@@ -27,6 +27,7 @@ namespace PhoneApp1._1__H
             client = new Service1Client();
             int x;
             int fafasfasd;
+            int bastiIsGay;
         }
 
         private void signUp_Click(object sender, RoutedEventArgs e)
