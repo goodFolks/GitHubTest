ba4ac94b5eb62c563cddef6a96b1c81483971c1e
Mahd AL-Basti <mahd.basti@hotmail.com>
2014-12-01 18:32:11 +0200

    hgkjkl

---
+++ PhoneApp1.1__H/PhoneApp1.1__H/writePost.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace PhoneApp1._1__H
+{
+    public partial class writePost : PhoneApplicationPage
+    {
+        public writePost()
+        {
+            InitializeComponent();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            myStoryboard.Begin();
+        }
+    }
+}
